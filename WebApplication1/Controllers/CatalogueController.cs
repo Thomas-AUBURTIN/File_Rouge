7af1d9456e6f8f201cfe9e9aa09fb169d8e2b039
Chatrio21 <thomas.auburtin@gmail.com>
2025-08-27 14:03:10 +0200

    Merge:travail_sur_edit

--- WebApplication1/Controllers/CatalogueController.cs
+++ WebApplication1/Controllers/CatalogueController.cs
@@ -178,5 +178,126 @@ namespace WebApplication1.Controllers
 
             }
         }
+        public EditeurJeuViewModel getTypesThemes(EditeurJeuViewModel singleJeux)
+        {
+
+
+
+            singleJeux.ListThemes.Add(new SelectListItem("--Choix tu trie--", ""));
+            singleJeux.ListTypes.Add(new SelectListItem("--Choix tu trie--", ""));
+            singleJeux.ListJoueur.Add(new SelectListItem("--Choix tu trie--", ""));
+            singleJeux.ListTemps.Add(new SelectListItem("--Choix tu trie--", ""));
+
+
+            {
+
+                using (var connexion = new NpgsqlConnection(_connexionString))
+                {
+                    string queryThemes = "select distinct j.themeid , t.nom from jeux_theme j   join THEME t on j.themeid=t.themeid order by themeid ";
+                    List<Theme> Themes = connexion.Query<Theme>(queryThemes).ToList();
+                    foreach (Theme Theme in Themes)
+                    {
+                        singleJeux.ListThemes.Add(new SelectListItem(Theme.nom, Theme.themeId.ToString()));
+                    }
+
+                }
+
+                using (var connexion = new NpgsqlConnection(_connexionString))
+                {
+                    string queryTypes = "select distinct j.TYPEID  , t.nom from JEUX_TYPE  j   join types t on j.typeid=t.typeid order by typeid ";
+                    List<JeuxType> Types = connexion.Query<JeuxType>(queryTypes).ToList();
+                    foreach (JeuxType type in Types)
+                    {
+                        singleJeux.ListTypes.Add(new SelectListItem(type.nom, type.typeId.ToString()));
+                    }
+
+
+                }
+
+                using (var connexion = new NpgsqlConnection(_connexionString))
+                {
+                    string queryTemps = "select distinct tempsjeumoyen from JEUX order by TEMPSJEUMOYEN  ";
+                    List<int> Temps = connexion.Query<int>(queryTemps).ToList();
+                    foreach (int temp in Temps)
+                    {
+                        singleJeux.ListTemps.Add(new SelectListItem(temp.ToString(), temp.ToString()));
+                    }
+
+
+                }
+                using (var connexion = new NpgsqlConnection(_connexionString))
+                {
+                    string queryJoueurs = "select distinct NOMBREJOUEURSRECOMMANDES  from JEUX order by NOMBREJOUEURSRECOMMANDES   ";
+                    List<int> Joueurs = connexion.Query<int>(queryJoueurs).ToList();
+                    foreach (int Joueur in Joueurs)
+                    {
+                        singleJeux.ListJoueur.Add(new SelectListItem(Joueur.ToString(), Joueur.ToString()));
+                    }
+
+
+                }
+                return singleJeux;
+
+
+            }
+        }
+        public IActionResult Detail(int id)
+        {
+
+            Jeux jeu;
+            string queryjeux = "select j.*,t1.*,t2.* ,c.* from jeux j join JEUX_THEME JT1 on j.JEUID = jt1.JEUID join theme t1 on t1.THEMEID  = jt1.THEMEID join JEUX_TYPE JT2 on j.JEUID = jt2.JEUID join types t2 on t2.typeID  = jt2.typeID join COMMENTAIRES C  on c.JEUID = j.JEUID  where j.JEUID = @id";
+            List<Jeux> jeux;
+            using (var connexion = new NpgsqlConnection(_connexionString))
+            {
+                try
+                {
+
+                    jeux = connexion.Query<Jeux, Theme, JeuxType, Commentaire, Jeux>(queryjeux,
+                    (jeu, theme, type, commentaire) =>
+                    {
+                        jeu.themes.Add(theme);
+                        jeu.types.Add(type);
+                        jeu.commentaires.Add(commentaire);
+
+                        return jeu;
+                    }, new { id = id },
+                    splitOn: "themeid,typeid,jeuid"
+                    ).ToList();
+
+                }
+                catch (System.Exception ex)
+                {
+                    return NotFound();
+                }
+
+                jeu = jeux.GroupBy(j => j.jeuid).Select(g =>
+            {
+                Jeux groupedJeu = g.First();
+                if (groupedJeu.commentaires.Count != 0)
+                {
+                    groupedJeu.commentaires = g.Select(j => j.commentaires.Single())
+                    .DistinctBy((c) => c.jeuId) // pour ne pas avoir de doublon sur la catégories on le distinct by 
+                                                .ToList();
+                }
+                if (groupedJeu.types.Count != 0)
+                {
+                    groupedJeu.types = g.Select(j => j.types.Single())
+                                                .DistinctBy((c) => c.typeId) // pour ne pas avoir de doublon sur la catégories on le distinct by 
+                                                .ToList();
+
+                }
+                if (groupedJeu.themes.Count != 0)
+                {
+                    groupedJeu.themes = g.Select(j => j.themes.Single())
+                                                .DistinctBy((c) => c.themeId) // pour ne pas avoir de doublon sur la catégories on le distinct by 
+                                                .ToList();
+
+                }
+                return groupedJeu;
+            }).First();
+            return View(jeu);
+        }
     }
+
+}
 }
